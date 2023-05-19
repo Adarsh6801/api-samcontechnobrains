0788ff80a7d73c6f42c0d77fcc0e5348a7b556b6
Adarsh <kpadarsh6801@gmail.com>
2023-05-19 11:39:18 +0530

    get all emails

--- controllers/userController/user.controller.js
+++ controllers/userController/user.controller.js
@@ -1,14 +1,17 @@
-
 import emailSheduleModel from "../../model/emailShedule.model.js";
 
-
-// shedule email
+// get all sheduled email
 export const getAllSheduledEmail = async (req, res) => {
-    try {
-      
-      res.status(200).send({ msg: "this is users data" });
-    } catch (error) {
-      console.log(error);
-      res.status(500).send({ msg: "Internal server error" });
+  try {
+    const getAllEmails = await emailSheduleModel.find({});
+    console.log(getAllEmails, "getAllEmails");
+    if (getAllEmails.length == 0) {
+      res.status(404).send({ msg: "Data is not found" });
+    } else {
+      res.status(200).send({ msg: "this is users data",getAllEmails });
     }
-  };
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ msg: "Internal server error" });
+  }
+};
--- routes/userRouter/user.router.js
+++ routes/userRouter/user.router.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import * as emailController from "../../controllers/userController/email.controller.js";
+import * as userController from "../../controllers/userController/user.controller.js"
 const router = express.Router();
 
 router.post('/shedule-email',emailController.emailShedule);
-router.post('/filter-email',emailController.filterEmail)
+router.post('/filter-email',emailController.filterEmail);
+
+router.get('/get-all-sheduled-email',userController.getAllSheduledEmail);
 
 export default router;
